4b8fd499f1d2de8301ea44fcd0e034735ddeb6b2
MulticolorWorld <multicolorworld.shinku@gmail.com>
2018-09-07 13:56:34 +0900

    fix typo

--- mastodon4j/src/main/java/com/sys1yagi/mastodon4j/api/entity/PushSubscription.kt
+++ mastodon4j/src/main/java/com/sys1yagi/mastodon4j/api/entity/PushSubscription.kt
@@ -5,7 +5,7 @@ import com.google.gson.annotations.SerializedName
 /**
  * see more https://github.com/tootsuite/documentation/blob/master/Using-the-API/API.md#push-subscription
  */
-class PushSubScription(
+class PushSubscription(
     @SerializedName("id") val id: Long = 0L,
     @SerializedName("endpoint") val endpoint: String = "",
     @SerializedName("server_key") val serverKey: String = ""
